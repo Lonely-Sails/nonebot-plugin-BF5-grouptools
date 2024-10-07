44c44f9870aa9e8c9c9a53115b0796d3679c9dad
LonelySail <xiaocaicai_email@sina.com>
2024-10-07 13:34:17 +0800

    修改：优化封禁记录显示。

--- nonebot_plugin_bf5_grouptools/__init__.py
+++ nonebot_plugin_bf5_grouptools/__init__.py
@@ -74,7 +74,8 @@ async def _(event: GroupMessageEvent, args: Message = CommandArg()):
         await query_ban_matcher.finish('查询超时，请稍后再试。', at_sender=True)
     if response:
         message_lines = []
-        for ban_info in response:
-            message_lines.append(json.dumps(ban_info, indent=4, ensure_ascii=False))
+        for index, ban_info in enumerate(response):
+            message_lines.append(F'{index + 1}.服务器 {ban_info['serverName']}')
+            message_lines.append(F'- 时间：{ban_info['createTime']}  原因：{ban_info['reason']}')
         await query_ban_matcher.finish('\n'.join(message_lines), at_sender=True)
-    await query_ban_matcher.finish(F'玩家 {args} 还没有被踢过。', at_sender=True)
+    await query_ban_matcher.finish(F'玩家 {args} 还没有被封禁过。', at_sender=True)
