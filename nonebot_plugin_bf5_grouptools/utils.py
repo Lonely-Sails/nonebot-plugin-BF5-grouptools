f35e56d1fae5ab4fdfe5a16be524a26445931938
LonelySail <xiaocaicai_email@sina.com>
2024-10-20 16:04:43 +0800

    更新：添加时区转换。

--- nonebot_plugin_bf5_grouptools/utils.py
+++ nonebot_plugin_bf5_grouptools/utils.py
@@ -4,4 +4,4 @@ from datetime import timedelta, datetime
 def format_time(utc: str):
     utc_time = datetime.strptime(utc, '%Y-%m-%dT%H:%M:%S.%fZ')
     beijing_time = utc_time + timedelta(hours=8)
-    return beijing_time.strftime('%Y-%m-%dT%H:%M:%S')
+    return beijing_time.strftime('%Y-%m-%d %H:%M:%S')
